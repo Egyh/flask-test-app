e62d66ec21a3b2228230aaf2d34cd5ab6c36bbe4
Egyh <yuhamaguchi18@gmail.com>
2023-12-23 20:50:34 +0900

    added new book index

--- flaskr/main.py
+++ flaskr/main.py
@@ -3,4 +3,18 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    books = [
+        {'title': 'Math',
+        'price': 1000,
+        'arrival_day': '2023/12/23'},
+        
+        {'title': 'English',
+        'price': 1500,
+        'arrival_day': '2023/12/27'},
+    ]
+    
+    return render_template(
+        'index.html',
+        books=books
+        )
+    
